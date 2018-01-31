b6b7b221d3d8704bd5d5b0f7936e7497867630f1
lokicherry <rlokesh@aconex.com>
2018-01-31 10:32:48 +0530

    adding register service worker

---
+++ src/main/resources/static/RegisterServiceWorkerInstall.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js')
+        .then(function(reg) {
+            console.log('Registration succeeded. Scope is ' + reg.scope);
+        })
+        .catch(function(error) {
+            console.error('Registration failed with ' + error);
+        });
+}
