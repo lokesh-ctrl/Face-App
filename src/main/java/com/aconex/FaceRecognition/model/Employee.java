86424ae8e243c1b9c94591c138e5e3d20e6bec46
siva <sraju728@gmail.com>
2018-01-31 10:46:21 +0530

    Creating Entity for Empolyee

---
+++ src/main/java/com/aconex/FaceRecognition/model/Employee.java
@@ -0,0 +1,38 @@
+package com.aconex.FaceRecognition.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Employee {
+    private String personId;
+    private String employeeId;
+    private String name;
+    private String designation;
+
+    public Employee(){
+
+    }
+    public Employee(String personId, String employeeId, String name, String designation){
+        this.personId = personId;
+        this.employeeId = employeeId;
+        this.name = name;
+        this.designation = designation;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+}
+
