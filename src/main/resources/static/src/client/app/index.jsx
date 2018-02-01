46a35058f868ff4185d7716095d2946f4e64d907
lokicherry <rlokesh@aconex.com>
2018-02-01 15:55:58 +0530

    adding instructrions

--- src/main/resources/static/src/client/app/index.jsx
+++ src/main/resources/static/src/client/app/index.jsx
@@ -5,7 +5,13 @@ import ReactDOM from 'react-dom'
 class App extends React.Component{
     render(){
         return(<div>
-                <div>Details of the person</div>
+                <div><h2><b>Instructions:</b></h2></div>
+                <ul>
+                <li>Take care while capturing, it should be center aligned</li>
+                    <li>Click on "SNAP" button to take the picture.</li>
+                    <li>Use "RETAKE" button, if any problems arises in step2 </li>
+                    <li>Click on "GET DETAILS" button to get the details.</li>
+                </ul>
             </div>
             )
 
