b5a7d5efdca4cd1e64ac4075a9f8b508dc86fc79
siva <sraju728@gmail.com>
2018-02-08 12:37:32 +0530

    Adding Status container and changing background colors

--- src/main/resources/static/src/client/app/index.jsx
+++ src/main/resources/static/src/client/app/index.jsx
@@ -23,16 +23,13 @@ class App extends React.Component{
 ReactDOM.render(<App />, document.getElementById("container2"));
 
 
-class Admin extends React.Component{
+class Status extends React.Component{
     render(){
         return(<div>
-                <div><h2><b>Instructions:</b></h2></div>
-                <ul>
-                    <li>Take care while capturing, it should be center aligned</li>
-                    <li>Click on "SNAP" button to take the picture.</li>
-                    <li>Use "RETAKE" button, if any problems arises in step2 </li>
-                    <li>Click on "Login" button to login into the admin's page.</li>
-                </ul>
+                <div><h2><b>Status:</b></h2></div>
+                <div id="myStatus">
+                    <div id="statusBar"></div>
+                </div>
             </div>
         )
 
@@ -41,5 +38,4 @@ class Admin extends React.Component{
     }
 }
 
-ReactDOM.render(<Admin />, document.getElementById("AdminContainer"));
-
+ReactDOM.render(<Status />, document.getElementById("container3"));
