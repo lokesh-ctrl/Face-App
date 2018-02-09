0b75c2f383309ccf3bba71b9b0f8915d16c9b657
manoj <smanoj@aconex.com>
2018-02-09 23:08:22 +0530

    Adding  ControlImg controller to control the requested image.

--- src/main/java/com/aconex/FaceRecognition/controller/ImageController.java
+++ src/main/java/com/aconex/FaceRecognition/controller/ImageController.java
@@ -1,12 +1,13 @@
 package com.aconex.FaceRecognition.controller;
 
 import com.aconex.FaceRecognition.representation.EmployeeDto;
+import com.aconex.FaceRecognition.representation.FindFaceStatusDto;
 import com.aconex.FaceRecognition.representation.ImageDto;
+import com.aconex.FaceRecognition.representation.PersonImageDto;
+import com.aconex.FaceRecognition.services.PersonFaceService;
+import jdk.nashorn.internal.runtime.FindProperty;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
 
@@ -14,28 +15,19 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import com.aconex.FaceRecognition.services.FaceDetector;
 
-
 @RestController
 @RequestMapping("/controlImg")
 public class ImageController {
 
-    private FaceDetector faceDetector;
-
-    @Autowired
-    public ImageController(FaceDetector faceDetector) {
-
-        this.faceDetector = faceDetector;
-    }
-
     @PostMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
-    public EmployeeDto recognizeFace(@RequestBody ImageDto imageDto) {
+    public FindFaceStatusDto DetectFace(@RequestBody ImageDto imageDto) {
 
         try {
 
-            byte[] imageBytes = Base64.getDecoder().decode(imageDto.getImage().substring(22));
-            EmployeeDto employeeDto = faceDetector.detectFace(imageBytes);
-            return employeeDto;
+            System.load("/home/manoj/Documents/Projects/Face-App/libs/libopencv_java320.so");
 
+            byte[] imageBytes = Base64.getDecoder().decode(imageDto.getImage().substring(22));
+            return PersonFaceService.FindFace(imageBytes);
 
         } catch (Exception e) {
             e.printStackTrace();
