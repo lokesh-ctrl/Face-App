8d049d656bfb0167aead48737fd8e90ca4b7e071
manoj <smanoj@aconex.com>
2018-02-12 10:39:16 +0530

    Added resource file paths to application.properties

--- src/main/java/com/aconex/FaceRecognition/controller/ImageController.java
+++ src/main/java/com/aconex/FaceRecognition/controller/ImageController.java
@@ -3,6 +3,7 @@ package com.aconex.FaceRecognition.controller;
 import com.aconex.FaceRecognition.representation.FindFaceStatusDto;
 import com.aconex.FaceRecognition.representation.ImageDto;
 import com.aconex.FaceRecognition.services.PersonFaceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.Base64;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -11,15 +12,20 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/controlImg")
 public class ImageController {
 
+    private PersonFaceService personFaceService;
+
+    @Autowired
+    ImageController(PersonFaceService personFaceService){
+        this.personFaceService=personFaceService;
+    }
+
     @PostMapping(produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE)
     public FindFaceStatusDto DetectFace(@RequestBody ImageDto imageDto) {
 
         try {
 
-            System.load("/home/manoj/Documents/Projects/Face-App/libs/libopencv_java320.so");
-
             byte[] imageBytes = Base64.getDecoder().decode(imageDto.getImage().substring(22));
-            return PersonFaceService.FindFace(imageBytes);
+            return personFaceService.FindFace(imageBytes);
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/aconex/FaceRecognition/services/PersonFaceService.java
+++ src/main/java/com/aconex/FaceRecognition/services/PersonFaceService.java
@@ -7,29 +7,44 @@ import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.objdetect.CascadeClassifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 
+
+@Service
 public class PersonFaceService {
 
+    @Value("${cascadefilePath}")
+    public String CascadeFile;
 
-    public static FindFaceStatusDto FindFace(byte[] imageBytes)throws IOException{
+    @Value("${sourceImagePath}")
+    public  String SourceImage;
+
+    @Value("${javaNativeLibraryPath}")
+    public String NativeLibrary;
+
+    public FindFaceStatusDto FindFace(byte[] imageBytes)throws IOException{
+
+        System.load(NativeLibrary);
 
         CascadeClassifier faceDetector = new CascadeClassifier();
-        faceDetector.load("/home/manoj/Documents/Projects/Face-App/src/main/resources/haarcascade_frontalface_alt.xml");
+        faceDetector.load(CascadeFile);
 
         ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
         BufferedImage bufferedImage = ImageIO.read(bis);
         bis.close();
 
-        File outputfile = new File("/home/manoj/face.png");
+        File outputfile = new File(SourceImage);
         ImageIO.write(bufferedImage, "png", outputfile);
 
 
-        Mat image = Imgcodecs.imread("/home/manoj/face.png");
+        Mat image = Imgcodecs.imread(SourceImage);
 
         MatOfRect faceDetections = new MatOfRect();
         faceDetector.detectMultiScale(image, faceDetections);
