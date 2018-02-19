0b0dd0c7846323623d6eadebdc3802ece7395451
lokicherry <rlokesh@aconex.com>
2018-02-19 10:19:48 +0530

    modyfing cam.js to show user what's heppening using statusText message

--- src/main/resources/static/src/client/public/js/cam.js
+++ src/main/resources/static/src/client/public/js/cam.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
     );
 
 
-    if(!navigator.getMedia){
+    if (!navigator.getMedia) {
         displayErrorMessage("Your browser doesn't have support for the navigator.getUserMedia interface.");
     }
     else navigator.getMedia(
@@ -40,6 +40,7 @@ document.addEventListener('DOMContentLoaded', function () {
             displayErrorMessage("There was an error with accessing the camera stream: " + err.name, err);
         }
     );
+
     function takeSnapshot() {
         updateStatusMessage("Streaming on");
         var hidden_canvas = document.querySelector('canvas'),
@@ -55,95 +56,96 @@ document.addEventListener('DOMContentLoaded', function () {
             return hidden_canvas.toDataURL('image/png');
         }
     }
+
     setTimeout(function () {
         takingSnap();
 
-    },1000);
+    }, 1000);
 
 
-    function takingSnap(){
+    function takingSnap() {
         var snap = takeSnapshot();
-        img=snap;
+        img = snap;
 
 
         image.setAttribute('src', snap);
 
-            var a={image:img};
-            $.ajax({
-                type: "POST",
-                url: "/controlImg",
-                contentType: 'application/json; charset=utf-8',
-                dataType: 'json',
-                data: JSON.stringify(a),
-                success: function(dataString) {
-                    if(dataString.status==="success"){
-                        updateStatus(20);
-                        updateStatusMessage("Person Found");
-                        document.getElementById("loader").style.visibility="visible";
-                        video.pause();
-                        updateStatus(40);
-                        updateStatusMessage("Recognizing Person..");
-                        $.ajax({
-                            type: "POST",
-                            url: "/recognize",
-                            contentType: 'application/json; charset=utf-8',
-                            dataType: 'json',
-                            data: JSON.stringify(a),
-                            success:function(detailsString){
-                                updateStatus(60);
-                                document.getElementById("loader").style.display = "none";
-                                detailsContainer=document.getElementById("container2");
-                                if(detailsString.employeeId===null) {
-                                    updateStatus(100);
-                                    updateStatusMessage("No details found in the database");
-                                    detailsContainer.innerHTML = "<div><h2>NO PERSON DETAILS FOUND.</h2></br><h3>Try Again (Or) Register</h3></br><button id='tryAgain'>TRY AGAIN</button><button id='register'>REGISTER</button></div>"
-                                    updateStatus(0);
-                                    document.getElementById("loader").style.visibility = "hidden";
+        var a = {image: img};
+        $.ajax({
+            type: "POST",
+            url: "/controlImg",
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(a),
+            success: function (dataString) {
+                if (dataString.status === "success") {
+                    updateStatus(20);
+                    updateStatusMessage("Person Found");
+                    document.getElementById("loader").style.visibility = "visible";
+                    video.pause();
+                    updateStatus(40);
+                    updateStatusMessage("Recognizing Person..");
+                    $.ajax({
+                        type: "POST",
+                        url: "/recognize",
+                        contentType: 'application/json; charset=utf-8',
+                        dataType: 'json',
+                        data: JSON.stringify(a),
+                        success: function (detailsString) {
+                            updateStatus(60);
+                            document.getElementById("loader").style.display = "none";
+                            detailsContainer = document.getElementById("container2");
+                            if (detailsString.employeeId === null) {
+                                updateStatus(100);
+                                updateStatusMessage("No details found in the database");
+                                detailsContainer.innerHTML = "<div><h2>NO PERSON DETAILS FOUND.</h2></br><h3>Try Again (Or) Register</h3></br><button id='tryAgain'>TRY AGAIN</button><button id='register'>REGISTER</button></div>"
+                                updateStatus(0);
+                                document.getElementById("loader").style.visibility = "hidden";
 
 
-                                    var tryAgain = document.getElementById("tryAgain");
-                                    var register = document.getElementById("register");
-                                    tryAgain.addEventListener('click', function () {
-                                        window.location = "../../../../index.html";
-                                    });
-                                    register.addEventListener('click', function () {
-                                        window.location = "/src/client/public/html/Register.html";
+                                var tryAgain = document.getElementById("tryAgain");
+                                var register = document.getElementById("register");
+                                tryAgain.addEventListener('click', function () {
+                                    window.location = "../../../../index.html";
+                                });
+                                register.addEventListener('click', function () {
+                                    window.location = "/src/client/public/html/Register.html";
 
-                                    });
-
-                                }
-                                else {
-                                    updateStatusMessage("Person Details Found");
-                                    updateStatus(100);
-                                    detailsContainer.innerHTML = "<div id='details'><h2>PERSON DETAILS:</h2> </br></br> <ul> <li>ID:" + detailsString.employeeId + "</li> <li>Name: " + detailsString.name + "</li> <li>Designation: " + detailsString.designation + "</li> </ul></div></<br><button id='openDoor'>OPEN DOOR</button></div>";
-                                    var opendoor = document.getElementById("openDoor");
-                                    opendoor.addEventListener('click', function (){
-
-
-                                        detailsContainer.innerHTML="<div id='instructions'><h2><b>Instructions:</b></h2><ul><li>Take care while capturing, it should be center aligned</li><li>Follow the status bar</li><li>After recognizing your face, it will show your details</li><li>Please click on opendoor button, to open the door.</li></ul></div>";
-                                        updateStatus(0);
-                                        video.play();
-                                        document.getElementById("loader").style.visibility="hidden";
-                                        setTimeout(function () {
-                                        takingSnap();
-                                    },1000);
                                 });
 
-                                }
-
-                    }})}
-                    else if(dataString.status=="Failure"){
-                        takingSnap();
-                        updateStatus(0);
-
-                    }
-
+                            }
+                            else {
+                                updateStatusMessage("Person Details Found");
+                                updateStatus(100);
+                                detailsContainer.innerHTML = "<div id='details'><h2>PERSON DETAILS:</h2> </br></br> <ul> <li>ID:" + detailsString.employeeId + "</li> <li>Name: " + detailsString.name + "</li> <li>Designation: " + detailsString.designation + "</li> </ul></div></<br><button id='openDoor'>OPEN DOOR</button></div>";
+                                var opendoor = document.getElementById("openDoor");
+                                opendoor.addEventListener('click', function () {
 
 
+                                    detailsContainer.innerHTML = "<div id='instructions'><h2><b>Instructions:</b></h2><ul><li>Take care while capturing, it should be center aligned</li><li>Follow the status bar</li><li>After recognizing your face, it will show your details</li><li>Please click on opendoor button, to open the door.</li></ul></div>";
+                                    updateStatus(0);
+                                    video.play();
+                                    document.getElementById("loader").style.visibility = "hidden";
+                                    setTimeout(function () {
+                                        takingSnap();
+                                    }, 1000);
+                                });
+
+                            }
+
+                        }
+                    })
+                }
+                else if (dataString.status == "Failure") {
+                    takingSnap();
+                    updateStatus(0);
 
                 }
 
-            });
+
+            }
+
+        });
 
     }
 
@@ -157,7 +159,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function displayErrorMessage(error_msg, error) {
         error = error || "";
-        if(error){
+        if (error) {
             console.error(error);
         }
 
@@ -175,13 +177,14 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("loader").style.display = "block";
     }
 
-});
+    function updateStatus(percentageStatus) {
+        status.style.width = percentageStatus + "%";
 
-function updateStatus(percentageStatus){
-    status.style.width = percentageStatus+"%";
+    }
 
+    function updateStatusMessage(message) {
+        statusMessageElement.innerText = message;
+    }
 }
+);
 
-function updateStatusMessage(message) {
-    statusMessageElement.innerText = message;
-}
