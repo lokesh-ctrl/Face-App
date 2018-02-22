08ecd4e01f0e099c2490b0eb9743d34191cc20c2
siva <sraju728@gmail.com>
2018-02-22 10:55:52 +0530

    adding David details in database

--- src/main/resources/db/migration/V1_1__create_employee.sql
+++ src/main/resources/db/migration/V1_1__create_employee.sql
@@ -12,4 +12,3 @@ insert into employee VALUES
   ('2825dfa6-38f6-4c77-84ad-1d66cb183490','intern-03','Lokesh','Intern in Engineering',0),
   ('c177f4be-e905-4fdb-bdf5-cd3c4116a7fc','intern-04','Harika','Intern in Engineering',0),
   ('3caf8e64-d7e0-45d2-829f-04b644c17f86','E01','David McNamara','UX Designer, EcoSystem',0);
-  ('3caf8e64-d7e0-45d2-829f-04b644c17f86','E-01','David McNamara','UX Designer,Ecosystem',0);
